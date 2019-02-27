85f1413a6c78d853f5b2f98fc4fa2d0eb261aa73
parrotgeek1 <parrotgeek1@gmail.com>
2019-02-27 12:38:52 -0800

    Attempt to fix LC_MAIN on 10.7

--- make.sh
+++ make.sh
@@ -2,6 +2,8 @@
 
 gcc -fPIC -O3 -Wall -Wextra -Werror -Wno-unused-parameter -arch x86_64 -dynamiclib -mmacosx-version-min=10.7 -Wl,-reexport_library,/usr/lib/libSystem.B.dylib -current_version 1 -compatibility_version 1 -o libFxShim.dylib shim.c
 
+gcc -fPIC -O3 -Wall -Wextra -Werror -Wno-sign-compare -arch x86_64 -mmacosx-version-min=10.7 -o trampoline trampoline.c
+
 mv libFxShim.dylib Firefox.app/Contents/MacOS/
 
 install_name_tool -change /usr/lib/libSystem.B.dylib '@loader_path/libFxShim.dylib' Firefox.app/Contents/MacOS/libnss3.dylib 
@@ -13,6 +15,12 @@ install_name_tool -change /usr/lib/libSystem.B.dylib '@loader_path/libFxShim.dyl
 sed -i '' 's/>10.9.0</>10.7.0</' Firefox.app/Contents/Info.plist
 v=`cat Firefox.app/Contents/Info.plist  | grep -A1 CFBundleShortVersionString | tail -n1 | cut -d '>' -f2 | cut -d '<' -f1`
 bash ./rebrand.sh
+
+find Firefox\ Legacy.app -type f -perm 0755 -not -name '*.dylib' | while read a; do 
+file "$a" | grep -q executable && (mv "$a" "${a}_real"; cp trampoline "$a"; codesign --deep -f -s 'Mac Developer' "$a"; codesign --deep -f -s 'Mac Developer' "${a}_real") || true
+done
+rm -f trampoline
+
 codesign --deep -f -s 'Mac Developer' Firefox\ Legacy.app
 rm -f Firefox\ Legacy\ v$v.zip 
 xattr -cr Firefox\ Legacy.app
---
+++ trampoline.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <dlfcn.h>
+#include <mach-o/dyld.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int EndsWith(const char *str, const char *suffix)
+{
+    if (!str || !suffix)
+        return 0;
+    size_t lenstr = strlen(str);
+    size_t lensuffix = strlen(suffix);
+    if (lensuffix >  lenstr)
+        return 0;
+    return strncmp(str + lenstr - lensuffix, suffix, lensuffix) == 0;
+}
+
+int callEntryPointOfImage(char *path, int argc, char **argv,char *envp[], char *apple[])
+{
+    void *handle;
+    int (*binary_main)(int binary_argc, char **binary_argv,char *envp[], char *apple[]);
+    char *error;
+    int err = 0;
+
+    handle = dlopen (path, RTLD_LAZY);
+    if (!handle) {
+        puts (dlerror());
+        err = 1;
+    }
+
+    uint64_t entryoff = 0;
+
+    /* Find LC_MAIN, entryoff */
+
+    uint32_t count = _dyld_image_count();
+
+    int didFind = 0;
+
+    for(uint32_t i = 0; i < count; i++)
+    {
+        //Name of image (includes full path)
+        const char *dyld = _dyld_get_image_name(i);
+        if (EndsWith(dyld, path))
+        {
+            didFind = 1;
+            const struct mach_header *header = (struct mach_header *)_dyld_get_image_header(i);
+
+            if (header->magic == MH_MAGIC_64)
+            {
+                const struct mach_header_64 *header64 = (struct mach_header_64 *)_dyld_get_image_header(i);
+
+                uint8_t *imageHeaderPtr = (uint8_t*)header64;
+                typedef struct load_command load_command;
+
+                imageHeaderPtr += sizeof(struct mach_header_64);
+                load_command *command = (load_command*)imageHeaderPtr;
+
+                for(int i = 0; i < header->ncmds > 0; ++i)
+                {
+                    if(command->cmd == LC_MAIN)
+                    {
+                        struct entry_point_command ucmd = *(struct entry_point_command*)imageHeaderPtr;
+
+                        entryoff = ucmd.entryoff;
+                        didFind = 1;
+                        break;
+                    }
+
+                    imageHeaderPtr += command->cmdsize;
+                    command = (load_command*)imageHeaderPtr;
+                }
+            }
+            else
+            {
+                const struct mach_header *header = (struct mach_header *)_dyld_get_image_header(i);
+
+                uint8_t *imageHeaderPtr = (uint8_t*)header;
+                typedef struct load_command load_command;
+
+                imageHeaderPtr += sizeof(struct mach_header);
+                load_command *command = (load_command*)imageHeaderPtr;
+
+                for(int i = 0; i < header->ncmds > 0; ++i)
+                {
+                    if(command->cmd == LC_MAIN)
+                    {
+                        struct entry_point_command ucmd = *(struct entry_point_command*)imageHeaderPtr;
+
+                        entryoff = ucmd.entryoff;
+                        didFind = 1;
+                        break;
+                    }
+
+                    imageHeaderPtr += command->cmdsize;
+                    command = (load_command*)imageHeaderPtr;
+                }
+            }
+
+            if (didFind)
+            {
+                break;
+            }
+        }
+    }
+
+    if (didFind)
+    {
+        binary_main = dlsym(handle, "_mh_execute_header")+entryoff;
+        if ((error = dlerror()) != NULL)  {
+            puts(error);
+            err = 1;
+        }
+
+        if (err == 0)
+        {
+            int i = (*binary_main)(argc, argv,envp,apple);
+            _exit(i);
+        }
+    }
+    return 1;
+}
+int main(int argc, char *argv[], char *envp[], char *apple[]) {
+    int sl = strlen(argv[0]);
+    char *argv0new = malloc(sl+6);
+    strcpy(argv0new,argv[0]);
+    strcat(argv0new,"_real");
+    return callEntryPointOfImage(argv0new,argc,argv,envp,apple);
+}
