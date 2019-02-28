0f770f8e5dda657b717cdd4efe0f307825f25e2d
parrotgeek1 <parrotgeek1@gmail.com>
2019-02-28 14:42:22 -0800

    Fix NSObject not found issue on 10.7

--- make.sh
+++ make.sh
@@ -4,6 +4,9 @@ cd unsign; make -s; cd ..
 
 gcc -fPIC -O3 -Wall -Wextra -Werror -Wno-unused-parameter -arch x86_64 -dynamiclib -mmacosx-version-min=10.7 -Wl,-reexport_library,/usr/lib/libSystem.B.dylib -current_version 1 -compatibility_version 1 -o libFxShim.dylib shim.c
 
+# NSObject was in CoreFoundation in Lion, not libobjc
+gcc -fPIC -O3 -Wall -Wextra -Werror -arch x86_64 -dynamiclib -mmacosx-version-min=10.7 -Wl,-reexport_library,/usr/lib/libobjc.A.dylib -Wl,-reexport_library,/System/Library/Frameworks/CoreFoundation.framework/Versions/A/CoreFoundation -current_version 1 -compatibility_version 1 -o libFxShimObjc.dylib shimObjc.c
+
 gcc -fPIC -O3 -Wall -Wextra -Werror -arch x86_64 -dynamiclib -mmacosx-version-min=10.7 -current_version 1 -compatibility_version 1 -framework CoreFoundation -o libFxShimVT.dylib shimVT.c
 
 gcc -fPIC -O3 -Wall -Wextra -Werror -Wno-sign-compare -arch x86_64 -mmacosx-version-min=10.7 -o trampoline trampoline.c
@@ -15,12 +18,22 @@ install_name_tool -change /usr/lib/libSystem.B.dylib '@loader_path/libFxShim.dyl
 install_name_tool -change /usr/lib/libSystem.B.dylib '@loader_path/libFxShim.dylib' Firefox.app/Contents/MacOS/firefox 
 
 install_name_tool -change /usr/lib/libSystem.B.dylib '@loader_path/libFxShim.dylib' Firefox.app/Contents/MacOS/XUL
+install_name_tool -change /usr/lib/libobjc.A.dylib '@loader_path/libFxShimObjc.dylib' Firefox.app/Contents/MacOS/XUL
 install_name_tool -change /System/Library/Frameworks/VideoToolbox.framework/Versions/A/VideoToolbox '@loader_path/libFxShimVT.dylib' Firefox.app/Contents/MacOS/XUL
 
 sed -i '' 's/>10.9.0</>10.7.0</' Firefox.app/Contents/Info.plist
 v=`cat Firefox.app/Contents/Info.plist  | grep -A1 CFBundleShortVersionString | tail -n1 | cut -d '>' -f2 | cut -d '<' -f1`
 p=`cat patch.txt`
 bash ./rebrand.sh $p $v
+
+#objc
+#Binary file ./Firefox Legacy.app/Contents/Library/LaunchServices/org.mozilla.updater_real matches
+#Binary file ./Firefox Legacy.app/Contents/MacOS/crashreporter.app/Contents/MacOS/crashreporter_real matches
+#Binary file ./Firefox Legacy.app/Contents/MacOS/updater.app/Contents/MacOS/org.mozilla.updater_real matches
+
+install_name_tool -change /usr/lib/libobjc.A.dylib '@loader_path/../../../libFxShimObjc.dylib' "Firefox Legacy.app/Contents/MacOS/crashreporter.app/Contents/MacOS/crashreporter"
+install_name_tool -change /usr/lib/libobjc.A.dylib '@loader_path/../../../libFxShimObjc.dylib' "Firefox Legacy.app/Contents/MacOS/updater.app/Contents/MacOS/org.mozilla.updater"
+rm -rf "Firefox Legacy.app/Contents/Library/LaunchServices"
 
 find Firefox\ Legacy.app -type f -perm 0755 -not -name '*.dylib' | while read a; do 
 file "$a" | grep -q executable && (mv "$a" "${a}_real"; cp trampoline "$a"; unsign/unsign "${a}_real"; cat "${a}_real.unsigned" > "${a}_real"; rm "${a}_real.unsigned"; perl -pi -e 's/\x28\x00\x00\x80/\x28\x00\x00\x00/' "${a}_real") || true
---
+++ shimObjc.c
@@ -0,0 +1,1 @@
+// We are only reexporting libraries, but there has to be a c file to compile
