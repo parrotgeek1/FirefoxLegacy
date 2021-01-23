a1e4a8d531ac4e67154b860f2ecdd1c2a37c9547
parrotgeek1 <parrotgeek1@gmail.com>
2021-01-23 01:49:54 -0800

    Remove useless echo

--- findUndefSyms.sh
+++ findUndefSyms.sh
@@ -8,7 +8,6 @@ cat /tmp/_fusym_tmp | sort | uniq > /tmp/_fusym_tmp_
 mv -f /tmp/_fusym_tmp_ /tmp/_fusym_tmp
 echo Generating used symbol list
 nm -u -m "$1/Contents/MacOS/XUL" "$1/Contents/MacOS/"*.dylib 2>/dev/null | grep -vF " weak " | grep -v "^$1" |grep -vF .objc_class_name_ | sed 's/.* external _/_/' | cut -d ' ' -f1 | grep . | sort | uniq > /tmp/_fusym_tmp2
-echo Checking symbols
 echo ---Undefined symbols---
 diff /tmp/_fusym_tmp2 /tmp/_fusym_tmp | grep '^< ' | cut -c3-
 rm -f /tmp/_fusym_tmp*
