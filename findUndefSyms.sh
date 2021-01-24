151407d64ad03a4f40168f4863a7876d0350f059
parrotgeek1 <parrotgeek1@gmail.com>
2021-01-23 20:23:06 -0800

    Fix phrasing

--- findUndefSyms.sh
+++ findUndefSyms.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-echo Generating OS symbol list step 1
+echo Generating available symbol list step 1
 nm -Uj /System/Library/Frameworks/*/Versions/*/* "$1"/Contents/MacOS/* "$1"/Contents/MacOS/*.app/Contents/MacOS/* `find "$1"/Contents/Resources -name '*.dylib' -type f` /System/Library/Frameworks/*/Versions/*/Frameworks/*/Versions/*/* /usr/lib/*.dylib /usr/lib/*/*.dylib 2>/dev/null > /tmp/_fusym_tmp
-echo Generating OS symbol list step 2
+echo Generating available symbol list step 2
 nm -Uj /System/Library/PrivateFrameworks/*/Versions/*/* /System/Library/PrivateFrameworks/*/Versions/*/Libraries/* /System/Library/PrivateFrameworks/*/Versions/*/Frameworks/*/Versions/*/* /System/Library/Frameworks/*/Versions/*/Libraries/* 2>/dev/null >> /tmp/_fusym_tmp
-echo Generating OS symbol list step 3
+echo Generating available symbol list step 3
 cat /tmp/_fusym_tmp | sort | uniq > /tmp/_fusym_tmp_
 mv -f /tmp/_fusym_tmp_ /tmp/_fusym_tmp
 echo Generating used symbol list
